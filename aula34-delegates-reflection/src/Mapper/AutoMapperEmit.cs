ec242450689b4e8477f737d55d772cf2b38f0e1f
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2017-01-02 21:53:09 +0000

    LoadMappers --- load IMapper objects from methods

--- aula34-delegates-reflection/src/Mapper/AutoMapperEmit.cs
+++ aula34-delegates-reflection/src/Mapper/AutoMapperEmit.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace Mapper
 {
@@ -15,6 +18,43 @@ namespace Mapper
                 .GetMethod("Build", Type.EmptyTypes)
                 .MakeGenericMethod(klassSrc, klassDest)
                 .Invoke(null, null);
+        }
+
+        /// <returns>Dictionary<Type, IMapper>  Where key is the type of the Src and the value is a Mapper</returns>
+        public static Dictionary<Type, IMapper> LoadMappers(string path) {
+            Dictionary<Type, IMapper> res = new Dictionary<Type, IMapper>();
+            IEnumerable<MethodInfo> ms = Assembly
+                .LoadFrom(path)                  // Assembly
+                .GetTypes()                      // Type[]
+                .SelectMany(t => t.GetMethods()) // IEnumerable<MethodInfo>
+                .Where(IsMapperMethod);          // IEnumerable<MethodInfo>
+            foreach (MethodInfo m in ms)
+            {
+                res.Add(m.GetParameters()[0].ParameterType, new MapperForMethod(m));
+            }
+            return res; 
+        }
+        public static bool IsMapperMethod(MethodInfo m)
+        {
+            if (!m.IsStatic || m.ReturnType == typeof(void)) return false;
+            return
+                Attribute.IsDefined(m, typeof(MapperAttribute))
+                && m.GetParameters().Length == 1;
+        }
+
+        class MapperForMethod : IMapper
+        {
+            private MethodInfo m;
+
+            public MapperForMethod(MethodInfo m)
+            {
+                this.m = m;
+            }
+
+            public object Map(object src)
+            {
+                return m.Invoke(null, new object[] { src });
+            }
         }
     }
 }
