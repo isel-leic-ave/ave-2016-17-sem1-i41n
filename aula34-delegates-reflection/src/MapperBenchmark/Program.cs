6dbdc2afd046c58a8d741223cb6bc0aecf9251af
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2017-01-02 14:32:59 +0000

    Add Concole Application and Benchmark

---
+++ aula34-delegates-reflection/src/MapperBenchmark/Program.cs
@@ -0,0 +1,24 @@
+﻿using Mapper;
+using MapperTest;
+using System;
+
+namespace MapperBenchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           School isel = new School("Isel", "Lisboa");
+           Student s = new Student { Nr = 27721, Name = "Ze Manel", School = isel };
+
+            MapperEmit<Student, Person> mapper = AutoMapperEmit
+                .Build<Student, Person>()
+                .Match("Nr", "Id")
+                .Match("School", "Org");
+
+            NBench.Bench(() => mapper.Map(s), "Mapping complex properties");
+            Person p = mapper.Map(s);
+            Console.ReadLine();
+        }
+    }
+}
